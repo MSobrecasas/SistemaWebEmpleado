8405b86542e8204b92f5273cc285fccec3eec4bc
MSobrecasas <sns.mary@hotmail.com>
2023-01-31 16:42:18 -0300

    Agregar archivos de proyecto.

---
+++ SistemaWebEmpleado/Models/Empleado.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaWebEmpleado.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50)]
+        public string Apellido { get; set; }
+
+
+        public string Legajo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Titulo { get; set; }
+    }
+}
